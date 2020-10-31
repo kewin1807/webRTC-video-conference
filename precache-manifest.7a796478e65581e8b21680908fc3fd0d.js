fb1a9361d179c80473d50415144ced058605f04f
Nguyen hung cuong <barcode.cuongnh@hblab.vn>
2020-10-31 11:26:46 +0700

    Updates

---
+++ precache-manifest.7a796478e65581e8b21680908fc3fd0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9051508f4e9585e54e48afe55bd99eb8",
+    "url": "/webRTC-video-conference/index.html"
+  },
+  {
+    "revision": "04128e8aa22e3ac806f0",
+    "url": "/webRTC-video-conference/static/css/2.e30c37b9.chunk.css"
+  },
+  {
+    "revision": "da68a9164d1f7a91f054",
+    "url": "/webRTC-video-conference/static/css/main.7e169c8b.chunk.css"
+  },
+  {
+    "revision": "04128e8aa22e3ac806f0",
+    "url": "/webRTC-video-conference/static/js/2.991c1332.chunk.js"
+  },
+  {
+    "revision": "71e9e4dedda4f381ea56e80067d4fca8",
+    "url": "/webRTC-video-conference/static/js/2.991c1332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da68a9164d1f7a91f054",
+    "url": "/webRTC-video-conference/static/js/main.0cab46ff.chunk.js"
+  },
+  {
+    "revision": "7db0aabceeb93b7485c6",
+    "url": "/webRTC-video-conference/static/js/runtime-main.07cdf87e.js"
+  }
+]);
